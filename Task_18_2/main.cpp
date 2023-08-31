d2746b2b55d61a9b0c30208edfbd940798abfb10
TimkyWimky <email@email.com>
2023-08-31 18:14:19 +0400

    Task_18_2

--- Task_18_2/main.cpp
+++ Task_18_2/main.cpp
@@ -1,5 +1,31 @@
 #include <iostream>
 
-int main(int, char**){
-    std::cout << "Hello, from Task_18_2!\n";
+int countWays(int n, int k = 3)
+{
+    int counter = 0;
+    
+    if(n == 0)
+        return 1;
+    else if(n < 0)
+        return 0;
+    
+    for(int i = 1; i <= k; i++)
+    {
+        counter += countWays(n-i, k);
+    }
+
+    return counter;
+}
+
+int main()
+{
+    int n = 0, k = 0;
+    std::cout   << "Enter the number of steps: ";
+    std::cin    >> n;
+    std::cout   << "Enter the max jump length: ";
+    std::cin    >> k;
+
+    std::cout   << "Numbers of ways: "
+                << countWays(n, k)
+                << std::endl;
 }
