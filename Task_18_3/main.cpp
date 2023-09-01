69e365b7ae5c1bdb146ce9d161965723387fb142
TimkyWimky <email@email.com>
2023-09-01 22:06:52 +0400

    finish task_18_3

--- Task_18_3/main.cpp
+++ Task_18_3/main.cpp
@@ -1,5 +1,35 @@
 #include <iostream>
 
-int main(int, char**){
-    std::cout << "Hello, from Task_18_3!\n";
+void evendigits(long long number, int* ans)
+{
+    if(number == 0)
+        return;
+    if(number%10%2 == 0)
+        (*ans)++;
+    number /= 10;
+    evendigits(number, ans);
+}
+
+long long inputData(void)
+{
+    long long number = 0;
+    std::cout   << "Enter the number: ";
+    std::cin    >> number;
+    return number;
+}
+
+void printResult(int* ans)
+{
+    std::cout   << "The number of even digits is equal to: "
+                << *ans
+                << std::endl;
+}
+
+int main()
+{
+    int ans = 0;
+    
+    evendigits(inputData(), &ans);
+    
+    printResult(&ans);
 }
